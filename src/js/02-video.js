e9146958d05f6a468e2512369c16d0ff1a2f133a
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-01-13 21:06:55 +0100

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// make Key for our value in localStorage
+const CUR_TIME = 'videoplayer-current-time';
+
+// Initial library vimeo
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = ({ seconds }) => {
+  // Save current time when whatching video
+  localStorage.setItem(CUR_TIME, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// Run video from current time
+if (localStorage[CUR_TIME]) {
+  player.setCurrentTime(localStorage.getItem(CUR_TIME));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+
+// Ключ локального сховища
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailData = document.querySelector('[name="email"]');
+const messageData = document.querySelector('[name="message"]');
+
+// Об'єкт, куди ми отримуємо значеннями полів
+const formData = {
+   email: ' ',
+   message: ' '
+};
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+
+// Зберігаємо значення полів у локальному сховищу після вводу значень
+function onFormInput(e) {
+   formData[e.target.name] = e.target.value;
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+   console.log(formData);
+}
+
+// Отримуємо значення збережених полів після перезавантаження сторінки
+function saveLocalStorage() {
+   const saveData = localStorage.getItem(STORAGE_KEY);
+   if (saveData) {
+      const saveValuesData = JSON.parse(saveData);
+      feedbackForm.elements.email.value = saveValuesData.email;
+      feedbackForm.elements.message.value = saveValuesData.message;
+   }
+}
+saveLocalStorage(); 
+
+// очищуємо сховище і поля форми після submit, виводимо у консоль значення полів.
+function onFormSubmit(e) {
+   e.preventDefault();
+   if (emailData.value && messageData.value) {
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+   }
+}
 
